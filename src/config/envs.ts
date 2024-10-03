cc217e1c735c6c845066b63ada80341b4062820f
Samuel <samuelviloriadd@gmail.com>
2024-10-03 15:13:33 -0500

    feat: docker and final features

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,15 +5,23 @@ import * as joi from "joi";
 interface envVars {
     PORT: number
     DATABASE_URL: string
+
+
+    NATS_SERVERS: string[]
 }
 
 const envsSchema = joi.object<envVars>({
     PORT: joi.number().required(),
-    DATABASE_URL: joi.string().required()
+    DATABASE_URL: joi.string().required(),
+    
+    NATS_SERVERS: joi.array().items(joi.string()).required()
 })
 .unknown(true);
 
-const { error, value } = envsSchema.validate(process.env);
+const { error, value } = envsSchema.validate({
+    ...process.env,
+    NATS_SERVERS: process.env.NATS_SERVERS?.split(",")
+});
 if(error) {
     throw new Error(`Config validation error: ${error.message}`);
 }
@@ -22,7 +30,9 @@ const envsVars: envVars = value;
 
 export const envs = {
     PORT: envsVars.PORT,
-    DATABASE_URL: envsVars.DATABASE_URL
+    DATABASE_URL: envsVars.DATABASE_URL,
+
+    NATS_SERVERS: envsVars.NATS_SERVERS
 }
 
 
